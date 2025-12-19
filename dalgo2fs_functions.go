9d8130ab109fb023c37ac64b9d60437052e351ca
Alexander Trakhimenok <alex@trakhimenok.com>
2025-12-19 10:30:44 +0000

    fix: imports sorted

--- dalgo2fs_functions.go
+++ dalgo2fs_functions.go
@@ -1,10 +1,11 @@
 package dalgo2firestore
 
 import (
-	"cloud.google.com/go/firestore"
 	"fmt"
-	"github.com/dal-go/dalgo/dal"
 	"strings"
+
+	"cloud.google.com/go/firestore"
+	"github.com/dal-go/dalgo/dal"
 )
 
 var keyToDocRef = func(key *dal.Key, client *firestore.Client) *firestore.DocumentRef {
@@ -30,4 +31,15 @@ var keyToCollectionRef = func(key *dal.Key, client *firestore.Client) *firestore
 		panic(fmt.Sprintf("collectionRef is nil for path=%s, key: %v", path, key))
 	}
 	return collectionRef
+}
+
+func GetFirestoreCollectionRef(colRef *dal.CollectionRef, client *firestore.Client) (fsCollectionRef *firestore.CollectionRef) {
+	if colRef == nil {
+		panic("colRef is a required parameter, got nil")
+	}
+	if client == nil {
+		panic("client is a required parameter, got nil")
+	}
+	path := colRef.Path()
+	return client.Collection(path)
 }
