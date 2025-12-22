77b83e6346afada91787ccd7e81736fe93c005c9
Alexander Trakhimenok <alex@trakhimenok.com>
2025-12-22 09:02:06 +0000

    fix(test): implemented test cases in TestGetFirestoreCollectionRef

--- dalgo2fs_functions_test.go
+++ dalgo2fs_functions_test.go
@@ -13,15 +13,58 @@ func TestGetFirestoreCollectionRef(t *testing.T) {
 		colRef *dal.CollectionRef
 		client *firestore.Client
 	}
+	client := &firestore.Client{}
+	rootColRef := dal.NewRootCollectionRef("c1", "id1")
+	subColRef := dal.NewCollectionRef("c2", "id2", dal.NewKeyWithID("c1", "id1"))
 	tests := []struct {
 		name                string
 		args                args
 		wantFsCollectionRef *firestore.CollectionRef
+		shouldPanic         bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "nil_colRef",
+			args: args{
+				colRef: nil,
+				client: client,
+			},
+			shouldPanic: true,
+		},
+		{
+			name: "nil_client",
+			args: args{
+				colRef: &rootColRef,
+				client: nil,
+			},
+			shouldPanic: true,
+		},
+		{
+			name: "root_collection",
+			args: args{
+				colRef: &rootColRef,
+				client: client,
+			},
+			wantFsCollectionRef: client.Collection("c1"),
+		},
+		{
+			name: "sub_collection",
+			args: args{
+				colRef: &subColRef,
+				client: client,
+			},
+			wantFsCollectionRef: client.Collection("c1/id1/c2"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				r := recover()
+				if tt.shouldPanic && r == nil {
+					t.Errorf("GetFirestoreCollectionRef() should have panicked")
+				} else if !tt.shouldPanic && r != nil {
+					t.Errorf("GetFirestoreCollectionRef() panicked unexpectedly: %v", r)
+				}
+			}()
 			if gotFsCollectionRef := GetFirestoreCollectionRef(tt.args.colRef, tt.args.client); !reflect.DeepEqual(gotFsCollectionRef, tt.wantFsCollectionRef) {
 				t.Errorf("GetFirestoreCollectionRef() = %v, want %v", gotFsCollectionRef, tt.wantFsCollectionRef)
 			}
